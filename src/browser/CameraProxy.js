af0dd4e327bc030ed0e1f43371a39738f2d92600
HikaruYasuda <prismic_lights@hotmail.co.jp>
2015-09-02 20:39:10 +0900

    Update CameraProxy.js

--- src/browser/CameraProxy.js
+++ src/browser/CameraProxy.js
@@ -1,4 +1,4 @@
-cordova.define("cordova-plugin-camera.CameraProxy", function(require, exports, module) { /*
+/*
  *
  * Licensed to the Apache Software Foundation (ASF) under one
  * or more contributor license agreements.  See the NOTICE file
@@ -167,5 +167,3 @@ module.exports = {
 };
 
 require("cordova/exec/proxy").add("Camera",module.exports);
-
-});
