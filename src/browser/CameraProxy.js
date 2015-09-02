9392b4940246ef30e7e82ecbc468dda5348a1171
HikaruYasuda <prismic_lights@hotmail.co.jp>
2015-09-02 20:34:31 +0900

    fix destinationType=DataURL

--- src/browser/CameraProxy.js
+++ src/browser/CameraProxy.js
@@ -1,4 +1,4 @@
-/*
+cordova.define("cordova-plugin-camera.CameraProxy", function(require, exports, module) { /*
  *
  * Licensed to the Apache Software Foundation (ASF) under one
  * or more contributor license agreements.  See the NOTICE file
@@ -20,8 +20,10 @@
  */
 
 function takePicture(success, error, opts) {
+    var encoding = opts[5],
+        destinationType = opts[1];
     if (opts && opts[2] === 1) {
-        capture(success, error);
+        capture(success, error, encoding, destinationType);
     } else {
         var input = document.createElement('input');
         input.type = 'file';
@@ -35,8 +37,14 @@ function takePicture(success, error, opts) {
                 input.parentNode.removeChild(input);
 
                 var imageData = readerEvent.target.result;
-
-                return success(imageData.substr(imageData.indexOf(',') + 1));
+                if (destinationType === Camera.DestinationType.FILE_URI) {
+                    var encoding = input.value.split('.').pop() === 'png' ? Camera.EncodingType.PNG : Camera.EncodingType.JPEG;
+                    getURI(imageData, encoding, function(url) {
+                        success(url);
+                    }, error);
+                } else {
+                    return success(imageData.substr(imageData.indexOf(',') + 1));
+                }
             }
 
             reader.readAsDataURL(inputEvent.target.files[0]);
@@ -46,7 +54,44 @@ function takePicture(success, error, opts) {
     }
 }
 
-function capture(success, errorCallback) {
+function getExt(encoding) {
+    switch (encoding) {
+        case Camera.EncodingType.JPEG:
+            return '.jpeg';
+        case Camera.EncodingType.PNG:
+        default:
+            return '.png';
+    }
+}
+function getURI(dataURL, encoding, cb, onError) {
+    var ext = getExt(encoding);
+    var blob = dataURI2Blob(dataURL);
+    webkitRequestFileSystem(window.TEMPORARY, 0, function(fs) {
+        var dir = 'camera/';
+        var fName =  + (new Date()-0) + ext;
+        fs.root.getDirectory(dir, {create:true}, function(dirEntry) {
+            dirEntry.getFile(fName, {create:true, exclusive:true}, function(entry) {
+                entry.createWriter(function(writer) {
+                    writer.onwrite = function () {
+                        cb(entry.toURL());
+                    };
+                    writer.onerror = onError;
+                    writer.write(blob);
+                }, onError);
+            }, onError);
+        }, onError);
+    }, onError);
+
+    function dataURI2Blob(dataURI) {
+        var split = dataURI.split(','), mime = split[0], byteString = atob(split[1]);
+        var mimeString = mime.split(':')[1].split(';')[0];
+        var i = 0, ii = byteString.length, ia = new Uint8Array(ii);
+        for (; i < ii; i++) ia[i] = byteString.charCodeAt(i);
+        return new Blob([ia], {type:mimeString});
+    }
+}
+
+function capture(success, errorCallback, encoding, destinationType) {
     var localMediaStream;
 
     var video = document.createElement('video');
@@ -62,15 +107,30 @@ function capture(success, errorCallback) {
         canvas.getContext('2d').drawImage(video, 0, 0, 320, 240);
         
         // convert image stored in canvas to base64 encoded image
-        var imageData = canvas.toDataURL('img/png');
-        imageData = imageData.replace('data:image/png;base64,', '');
+        //var imageData = canvas.toDataURL('img/png');
+        //imageData = imageData.replace('data:image/png;base64,', '');
 
         // stop video stream, remove video and button
         localMediaStream.stop();
         video.parentNode.removeChild(video);
         button.parentNode.removeChild(button);
 
-        return success(imageData);
+        if (destinationType === Camera.DestinationType.FILE_URI) {
+            function getMime(encoding) {
+                switch (encoding) {
+                    case Camera.EncodingType.JPEG:
+                        return 'image/jpeg';
+                    case Camera.EncodingType.PNG:
+                    default:
+                        return 'image/png';
+                }
+            }
+            getURI(canvas.toDataURL(getMime(encoding)), encoding, function(url) {
+                success(url);
+            }, errorCallback);
+        } else {
+            return success(imageData);
+        }
     }
 
     navigator.getUserMedia = navigator.getUserMedia ||
@@ -96,7 +156,16 @@ function capture(success, errorCallback) {
 
 module.exports = {
     takePicture: takePicture,
-    cleanup: function(){}
+    cleanup: function(success, error){
+        webkitRequestFileSystem(window.TEMPORARY, 0, function(fs) {
+            var dir = 'camera/';
+            fs.root.getDirectory(dir, {create:true}, function(dirEntry) {
+                dirEntry.removeRecursively(success, error);
+            }, error);
+        }, error);
+    }
 };
 
 require("cordova/exec/proxy").add("Camera",module.exports);
+
+});
